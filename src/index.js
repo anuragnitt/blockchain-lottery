563873749696de609989c2485a78aff77911b038
Anurag Goyal <anuraggoyal.awr@gmail.com>
2022-12-07 18:52:20 +0530

    Minor fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CryptoLottery } from './lottery';
+import { Lottery } from './lottery';
 import './styles/index.css';
 
 ReactDOM.render(
